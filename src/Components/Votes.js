0e20f3b9c0256da13b36f16fde150a94cf153326
WillWatkins <w.watkins@hotmail.co.uk>
2021-12-22 15:45:01 +0000

    Change vote colour based on how the user votes

--- src/Components/Votes.js
+++ src/Components/Votes.js
@@ -5,36 +5,40 @@ export const Votes = ({ reviewVotes, reviewId }) => {
   const [votes, setVotes] = useState(reviewVotes);
   const [trackVote, setTrackVote] = useState(0);
 
+  const handleUpVote = () => {
+    if (trackVote < 1) {
+      updateVote(reviewId, { inc_votes: 1 }).then(() => {
+        setVotes((prevVotes) => {
+          return prevVotes + 1;
+        });
+      });
+      setTrackVote((prevValue) => prevValue + 1);
+    }
+  };
+
+  const handleDownVote = () => {
+    if (trackVote > -1) {
+      updateVote(reviewId, { inc_votes: -1 }).then(() => {
+        setVotes((prevVotes) => {
+          return prevVotes - 1;
+        });
+      });
+      setTrackVote((prevValue) => prevValue - 1);
+    }
+  };
+
   return (
     <h4 className="Votes">
       <p
-        className="UpVote"
-        onClick={() => {
-          if (trackVote < 1) {
-            updateVote(reviewId, { inc_votes: 1 }).then(() => {
-              setVotes((prevVotes) => {
-                return prevVotes + 1;
-              });
-            });
-            setTrackVote((prevValue) => prevValue + 1);
-          }
-        }}
+        className={trackVote === 1 ? "UpVoteActive" : "UpVote"}
+        onClick={handleUpVote}
       >
         ⬆{" "}
       </p>
       <p className="Vote"> {votes}</p>
       <p
-        className="DownVote"
-        onClick={() => {
-          if (trackVote > -1) {
-            updateVote(reviewId, { inc_votes: -1 }).then(() => {
-              setVotes((prevVotes) => {
-                return prevVotes - 1;
-              });
-            });
-            setTrackVote((prevValue) => prevValue - 1);
-          }
-        }}
+        className={trackVote === -1 ? "DownVoteActive" : "DownVote"}
+        onClick={handleDownVote}
       >
         ⬇
       </p>
